af16133f7a53ae5a350e31b33ec57a7c17e3a6f1
sarath8105@gmail.com <sarath8105@gmail.com>
2025-08-08 17:48:41 +0530

    My first commit

---
+++ src/main/java/com/example/eticketbooking/EticketbookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.eticketbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EticketbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EticketbookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eticketbooking/EticketbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eticketbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EticketbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
