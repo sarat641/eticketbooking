29b74c192036070f16c462144076307fc3324248
sarath8105@gmail.com <sarath8105@gmail.com>
2025-08-10 23:56:47 +0530

    Added Junit Test cases

---
+++ src/test/java/com/example/eticketbooking/util/TestUtil.java
@@ -0,0 +1,24 @@
+package com.example.eticketbooking.util;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.http.MediaType;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public class TestUtil {
+    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8")
+    );
+
+    public static byte[] convertObjectToJsonBytes(Object object) throws IOException, JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return mapper.writeValueAsBytes(object);
+    }
+}
